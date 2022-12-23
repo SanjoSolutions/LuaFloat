641478f0dc083649580bb38d66a2f5c8ad54d68e
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-23 11:05:03 +0100

    Enables add-on for "Library"

--- Float.lua
+++ Float.lua
@@ -1,5 +1,11 @@
-Float = {}
+local addOnName = ...
+local version = '1.0.0'
 
-function Float.seemsCloseBy(a, b)
-  return math.abs(b - a) < 0.0000000001
+if not Library.isRegistered(addOnName, version) then
+  --- @class Float
+  local Float = {}
+
+  function Float.seemsCloseBy(a, b)
+    return math.abs(b - a) < 0.0000000001
+  end
 end

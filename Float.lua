bd2c3b8049d0af6bdfac7a8435052d25bd49ea84
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-14 17:09:34 +0200

    Work on using Library in library packages

--- Float.lua
+++ Float.lua
@@ -1,17 +1,17 @@
 local addOnName = 'Float'
-local version = '1.0.0'
+local version = '2.0.0'
 
-if (_G.Library and not Library.isRegistered(addOnName, version)) or not _G.Library then
-  --- @class Float
-  local Float = {}
+if _G.Library then
+  if not Library.isRegistered(addOnName, version) then
+    --- @class Float
+    local Float = {}
 
-  function Float.seemsCloseBy(a, b)
-    return math.abs(b - a) < 0.0000000001
-  end
+    function Float.seemsCloseBy(a, b)
+      return math.abs(b - a) < 0.0000000001
+    end
 
-  if _G.Library then
     Library.register(addOnName, version, Float)
-  else
-    _G.Float = Float
   end
+else
+  error(addOnName + ' requires Library. It seems absent.')
 end
